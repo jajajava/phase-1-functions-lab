5e68b9bf0fbf223cf3ed403a1d19b8c7d58c4d2e
David <davidstepanov@yahoo.com>
2022-08-30 10:23:22 -0400

    commit tree cd6735cbd7096c9e54eced9b9e2a417192cef3a0

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  return Math.abs(42 - someValue);
+}
+distanceFromHqInBlocks(50);
+
+
+
+function distancefromHqInFeet(someValue) {
+  let blocks = distanceFromHqInBlocks(someValue)
+  return 264 * blocks;
+}
+console.log(distancefromHqInFeet(43));
+
+function distanceTravelledInFeet(start,destination){
+  return (Math.abs(start-destination))*264;
+}
+ distanceTravelledInFeet(5, 1)
+
+function calculatesFarePrice(start,destination){
+  let distance = distanceTravelledInFeet(start, destination)
+  if(distance < 400){
+    return 0
+  } else if (distance > 400 && distance < 2000){
+    return (distance - 400)*0.02
+  } else if (distance > 2000 && distance < 2500){
+    return 25
+  } else {
+    return "cannot travel that far"
+  }
+}
+calculatesFarePrice(15,20)
+calculatesFarePrice(0,500)
+calculatesFarePrice(0,2200)
+calculatesFarePrice(0,2600)
